05772130fbcc26649e208f2e0a094aa4302c410d
yoggy <yoggy0@gmail.com>
2014-11-21 20:08:38 +0900

    modified

--- README.md
+++ README.md
@@ -6,54 +6,55 @@ cv::goodFeaturesToTrack()とcv::calcOpticalFlowPyrLK()を使用し、
 検出した特徴点のオプティカルフローを追跡。
 
 <pre>
+class OpticalFlow_GoodFeature {
+public:
+        ・
+        ・
+        ・
+    void append_features(const cv::Mat &img) {
+        if (img.empty()) return;
 
-void append_features(const cv::Mat &gray_img, std::vector&lt;cv::Point2f> &features)
-{
-    std::vector&lt;cv::Point2f> points;
-    cv::goodFeaturesToTrack(gray_img, points, MAX_COUNT, 0.01, 10, cv::Mat(), 3, 0, 0.04);
-    features.insert(features.end(), points.begin(), points.end());
-}
+        cv::Mat target_img = img;
+        if (target_img.type() == CV_8UC3) {
+            cv::cvtColor(target_img, target_img, CV_BGR2GRAY);
+        }
 
-int main(int argc, char* argv[])
-{
-    ・
-    ・
-    ・
+        cv::vector&lt;cv::Point2f> results;
+        cv::goodFeaturesToTrack(target_img,results, max_count_, 0.01, 10, cv::Mat(), 3, 0, 0.04);
+
+        features1_.insert(features1_.end(), results.begin(), results.end());
+    }
+    
+    void calc(const cv::Mat &img) {
+        ・
+        ・
+        ・
+        // 履歴処理
+        features0_ = features1_;
+        features1_.clear();
+
+        frame1_.copyTo(frame0_);
+        frame1_ = target_img;
+        if (frame0_.empty()) frame0_ = target_img;
+
+        cv::calcOpticalFlowPyrLK(frame0_, frame1_, features0_, features1_, 
+            status_, err_, winsize_, 3, term_criteria_, 0, epsilon_);
+    }
+        ・
+        ・
+        ・
+int main(int argc, char* argv[]) {
+    OpticalFlow_GoodFeature optical_flow;
+
+    cv::VideoCapture capture;
+    capture.open(0);
+
     while(true) {
-        // 1フレーム前の状態を保存
-        features0 = features1;
-        features1.clear();
-
-        frame1_gray.copyTo(frame0_gray);
-        capture >> frame1_rgb;
-        cv::cvtColor(frame1_rgb, frame1_gray, CV_BGR2GRAY);
-
-        if (features0.size() == 0) {
-            append_features(frame1_gray, features1);
-        }
-        else {
-            std::vector&lt;uchar> status;
-            std::vector&lt;float> err;
-
-            // 特徴点のオプティカルフローを計算
-            cv::calcOpticalFlowPyrLK(frame0_gray, frame1_gray, features0, features1,
-                status, err, winsize, 3, term_criteria, 0, epsilon);
-
-            // トラッキングできた特徴点のみ残す
-            int count = 0;
-            for (unsigned int i = 0; i &lt; features1.size(); ++i) {
-                if (status[i]) {
-                    features1[count++] = features1[i];
-                }
-            }
-            features1.resize(count);
-
-            // 特徴点が少ない場合は追加しておく
-            if (features1.size() &lt; MIN_COUNT) {
-                append_features(frame1_gray, features1);
-            }
-        }
-    ・
-    ・
-    ・
+        cv::Mat capture_img;
+        capture >> capture_img;
+        optical_flow.calc(capture_img);
+        ・
+        ・
+        ・
+    }
 </pre>
--- cv_opticalflow_prylk.cpp
+++ cv_opticalflow_prylk.cpp
@@ -24,21 +24,103 @@
 #endif
 #endif
 
-#define MAX_COUNT 500
-#define MIN_COUNT 300
+// オプティカルフロー格納クラス
+class OpticalFlow_GoodFeature {
+public:
+	OpticalFlow_GoodFeature() 
+		: max_count_(1000), min_count_(300), 
+		winsize_(31, 31),  
+		term_criteria_(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 20, 0.03), 
+		epsilon_(0.00001)
+	{
+	}
 
-void append_features(const cv::Mat &gray_img, std::vector<cv::Point2f> &features)
-{
-	std::vector<cv::Point2f> points;
-	cv::goodFeaturesToTrack(gray_img, points, MAX_COUNT, 0.01, 10, cv::Mat(), 3, 0, 0.04);
+	void clear() {
+		frame0_.release();
+		frame1_.release();
+		features0_.clear();
+		features1_.clear();
+		status_.clear();
+		err_.clear();
+	}
 
-	features.insert(features.end(), points.begin(), points.end());
-}
+	void append_features(const cv::Mat &img) {
+		if (img.empty()) return;
+
+		cv::Mat target_img = img;
+		if (target_img.type() == CV_8UC3) {
+			cv::cvtColor(target_img, target_img, CV_BGR2GRAY);
+		}
+
+		cv::vector<cv::Point2f> results;
+		cv::goodFeaturesToTrack(target_img,results, max_count_, 0.01, 10, cv::Mat(), 3, 0, 0.04);
+
+		features1_.insert(features1_.end(), results.begin(), results.end());
+	}
+
+	void calc(const cv::Mat &img) {
+		cv::Mat target_img = img;
+		if (target_img.type() == CV_8UC3) {
+			cv::cvtColor(target_img, target_img, CV_BGR2GRAY);
+		}
+
+		// トラッキングできた特徴点を前に詰めてリサイズ
+		int count = 0;
+		for (unsigned int i = 0; i < status_.size(); ++i) {
+			if (status_[i]) {
+				features1_[count++] = features1_[i];
+			}
+		}
+		features1_.resize(count);
+
+		// 特徴点が少ない場合は追加しておく
+		if ((int)features1_.size() < min_count_) {
+			append_features(target_img);
+		}
+
+		// 履歴処理
+		features0_ = features1_;
+		features1_.clear();
+
+		frame1_.copyTo(frame0_);
+		frame1_ = target_img;
+		if (frame0_.empty()) frame0_ = target_img;
+
+		cv::calcOpticalFlowPyrLK(frame0_, frame1_, features0_, features1_, 
+			status_, err_, winsize_, 3, term_criteria_, 0, epsilon_);
+	}
+
+	void draw(cv::Mat &canvas) const {
+		for (unsigned int i = 0; i < status_.size(); ++i) {
+			if (status_[i]) {
+				cv::Point p0 = features0_[i];
+				cv::Point p1 = features1_[i];
+
+				cv::line(canvas, p0, p1, CV_RGB(0,255,0), 1);
+				cv::circle(canvas, p1, 2, CV_RGB(255,0,0), CV_FILLED);
+			}
+		}
+	}
+
+protected:
+	int max_count_;
+	int min_count_;
+	cv::Mat frame0_;
+	cv::Mat frame1_;
+	std::vector<cv::Point2f> features0_;
+	std::vector<cv::Point2f> features1_;
+	std::vector<uchar> status_;
+	std::vector<float> err_;
+
+	cv::Size winsize_;
+	cv::TermCriteria term_criteria_;
+	double epsilon_;
+};
 
 int main(int argc, char* argv[])
 {
-	cv::Mat frame0_gray, frame1_gray, frame1_rgb;
-	std::vector<cv::Point2f> features0, features1;
+	OpticalFlow_GoodFeature optical_flow;
+	cv::Mat capture_img;
 
 	cv::VideoCapture capture;
 	if (capture.open(0) == false) {
@@ -46,61 +128,22 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	cv::Size winsize(31, 31);
-	cv::TermCriteria term_criteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 20, 0.03);
-	double epsilon = 0.00001;
-
 	while(true) {
-		// 1フレーム前の状態を保存
-		features0 = features1;
-		features1.clear();
+		capture >> capture_img;
+		optical_flow.calc(capture_img);
 
-		frame1_gray.copyTo(frame0_gray);
-		capture >> frame1_rgb;
-		cv::cvtColor(frame1_rgb, frame1_gray, CV_BGR2GRAY);
-
-		if (features0.size() == 0) {
-			append_features(frame1_gray, features1);
-		}
-		else {
-			std::vector<uchar> status;
-			std::vector<float> err;
-
-			// 特徴点のオプティカルフローを計算
-			cv::calcOpticalFlowPyrLK(frame0_gray, frame1_gray, features0, features1,
-				status, err, winsize, 3, term_criteria, 0, epsilon);
-
-			// トラッキングできた特徴点のみ残す
-			int count = 0;
-			for (unsigned int i = 0; i < features1.size(); ++i) {
-				if (status[i]) {
-					features1[count++] = features1[i];
-				}
-			}
-			features1.resize(count);
-
-			// 特徴点が少ない場合は追加しておく
-			if (features1.size() < MIN_COUNT) {
-				append_features(frame1_gray, features1);
-			}
-		}
-
-		// 結果の描画
-		cv::Mat canvas(frame1_rgb.size(), CV_8UC3);
-		frame1_rgb.copyTo(canvas);
-
-		for (unsigned int i = 0; i < features1.size(); ++i) {
-			cv::circle(canvas, features1[i], 1, CV_RGB(0,255,0), CV_FILLED);
-		}
+		cv::Mat canvas;
+		capture_img.copyTo(canvas);
+		optical_flow.draw(canvas);
 
 		cv::imshow("cv_opticalflow_prylk", canvas);
 
 		int c = cv::waitKey(1);
 		if (c == ' ') {
-			append_features(frame1_gray, features1);
+			optical_flow.append_features(capture_img);
 		}
 		else if (c == 'c') {
-			features1.clear();
+			optical_flow.clear();
 		}
 		else if (c == 27) {
 			break;
